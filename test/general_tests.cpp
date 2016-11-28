8d1283e7ea4a9fc1c57c9aac433b6e2a43a03771
Markus Klemm <markus@markus@markusklemm.net>
2016-11-28 23:16:28 +0100

    Imporved testing

--- test/general_tests.cpp
+++ test/general_tests.cpp
@@ -3,16 +3,11 @@
 
 #include <dist_vec_ret.hpp>
 
-BOOST_AUTO_TEST_CASE(always_true)
-{
-  int i = 1;
-  BOOST_TEST(i);
-  BOOST_TEST(i == 1);
-}
+
 
 BOOST_AUTO_TEST_CASE(dist_vec_ret_init){
     information_retrieval::indexer index1("This is a test query");
-    BOOST_FAIL(index1.isindexed());
+    BOOST_CHECK(!index1.isindexed());
     index1.index();
     BOOST_CHECK(index1.isindexed());
 
