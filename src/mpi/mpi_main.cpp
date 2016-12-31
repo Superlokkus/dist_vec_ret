022ce8888ade05ab02c0ef8d37a1b25b774555f2
Markus Klemm <markus@markus@markusklemm.net>
2016-12-31 18:18:34 +0100

    first own mpi main

--- src/mpi/mpi_main.cpp
+++ src/mpi/mpi_main.cpp
@@ -1,62 +1,26 @@
-/*
-  "Hello World" MPI Test Program
-*/
-#include <assert.h>
-#include <stdio.h>
-#include <string.h>
+/*! @file mpi_main.cpp
+ *
+ */
+
 #include <mpi.h>
+#include <iostream>
+#include <exception>
+#include <cstdlib>
 
-int main(int argc, char **argv)
+
+[[noreturn]] static void terminate_with_mpi_abort() {
+    MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+    abort();
+}
+
+int main(int argc, char *argv[])
 {
-    char buf[256];
-    int my_rank, num_procs;
+    int supported = 0;
+    if (MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &supported) != MPI_SUCCESS)//MPI_THREAD_SINGLE
+        return EXIT_FAILURE;
+    std::cout << "Mutiple is " << MPI_THREAD_MULTIPLE << " supported is " << supported << std::endl;
 
-    /* Initialize the infrastructure necessary for communication */
-    MPI_Init(&argc, &argv);
-
-    /* Identify this process */
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-
-    /* Find out how many total processes are active */
-    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
-
-    /* Until this point, all programs have been doing exactly the same.
-       Here, we check the rank to distinguish the roles of the programs */
-    if (my_rank == 0) {
-        int other_rank;
-        printf("We have %i processes.\n", num_procs);
-
-        /* Send messages to all other processes */
-        for (other_rank = 1; other_rank < num_procs; other_rank++)
-        {
-            sprintf(buf, "Hello %i!", other_rank);
-            MPI_Send(buf, sizeof(buf), MPI_CHAR, other_rank,
-                     0, MPI_COMM_WORLD);
-        }
-
-        /* Receive messages from all other process */
-        for (other_rank = 1; other_rank < num_procs; other_rank++)
-        {
-            MPI_Recv(buf, sizeof(buf), MPI_CHAR, other_rank,
-                     0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            printf("%s\n", buf);
-        }
-
-    } else {
-
-        /* Receive message from process #0 */
-        MPI_Recv(buf, sizeof(buf), MPI_CHAR, 0,
-                 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        assert(memcmp(buf, "Hello ", 6) == 0),
-
-        /* Send message to process #0 */
-        sprintf(buf, "Process %i reporting for duty.", my_rank);
-        MPI_Send(buf, sizeof(buf), MPI_CHAR, 0,
-                 0, MPI_COMM_WORLD);
-
-    }
-
-    /* Tear down the communication infrastructure */
     MPI_Finalize();
+
     return 0;
 }
