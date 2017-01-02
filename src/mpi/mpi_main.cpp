05595300a06ee21987981340d1270d297dca4a3a
Markus Klemm <markus@markus@markusklemm.net>
2017-01-02 14:49:50 +0100

    Proper MPI initialization and cleanup

--- src/mpi/mpi_main.cpp
+++ src/mpi/mpi_main.cpp
@@ -15,10 +15,13 @@
 
 int main(int argc, char *argv[])
 {
-    int supported = 0;
-    if (MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &supported) != MPI_SUCCESS)//MPI_THREAD_SINGLE
+    int provided = 0;
+    if (MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &provided) != MPI_SUCCESS)
         return EXIT_FAILURE;
-    std::cout << "Mutiple is " << MPI_THREAD_MULTIPLE << " supported is " << supported << std::endl;
+
+    std::set_terminate(&terminate_with_mpi_abort);
+
+
 
     MPI_Finalize();
 
